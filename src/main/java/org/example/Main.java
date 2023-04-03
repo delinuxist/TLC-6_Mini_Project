96618ca6d9ce425a2466c0ff89634ad1c911d755
Emmanuel Obeng Twene <emmanuel.twene@turntabl.io>
2023-04-03 17:41:23 +0000

    just testing some classes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,31 @@
 package org.example;
 
+import org.example.card.Card;
+import org.example.deck.Deck;
+import org.example.game.Game;
+import org.example.player.Player;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+    Game game =  new Game(10);
+
+    for(Player i: game.getPlayers()){
+        System.out.println(i);
+    }
+
+//    for(Card i : game.getDeck().getDeck()) {
+//        System.out.println(i.getRank().getRank() +" " +" "+i.getSuit());
+//    }
+
+//        System.out.println("<<<<Shuffled>>>>>");
+//        for(Card i : game.getDeck().getDeck()) {
+//            System.out.println(i);
+//        }
+//
+//        for(int i = 0; i<3;i++) {
+//            System.out.println(game.getDeck().getDeck().remove(0));
+//
+//        }
+//        System.out.println(game.getDeck().getDeck().size());
     }
 }
